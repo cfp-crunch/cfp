b5f8e5dd032bfb5f0b67bff523093796ce30c015
dBob <bobrovnykov@yahoo.com>
2025-03-05 13:59:18 +0700

    First commit

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'cfp_crunch',
+      short_name: 'CFP',
+      description: 'Crunching some CFP data',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: true,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+  base: '/'
+})
